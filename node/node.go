9bf3e251a17ad816cc5f52497bbba9269bf41b24
christopherbrumm <christopherbrumm2000@gmail.com>
2023-05-26 12:10:42 +0200

    update: 6

--- node/node.go
+++ node/node.go
@@ -50,29 +50,31 @@ func GetNodeHeight() int {
 
 	if err != nil {
 		fmt.Print(err.Error())
-		os.Exit(1)
+		fmt.Println("Failed to query height. Try again in 5s ...")
+		time.Sleep(time.Second * 5)
+		GetNodeHeight()
+	} else {
+		responseData, err := io.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		var resp Response
+		err = json.Unmarshal([]byte(responseData), &resp)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		lastBlockHeight := resp.Result.Response.LastBlockHeight
+
+		nodeHeight, err := strconv.Atoi(lastBlockHeight)
+
+		if err != nil {
+			fmt.Println("Error during conversion", err)
+		}
+
+		fmt.Println("Succesfully retrieved node height: ", nodeHeight)
+
+		return nodeHeight
 	}
-
-	responseData, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	var resp Response
-	err = json.Unmarshal([]byte(responseData), &resp)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	lastBlockHeight := resp.Result.Response.LastBlockHeight
-
-	nodeHeight, err := strconv.Atoi(lastBlockHeight)
-
-	if err != nil {
-		fmt.Println("Error during conversion", err)
-	}
-
-	fmt.Println("GetNodeHeight: ", nodeHeight)
-
-	return nodeHeight
 }
