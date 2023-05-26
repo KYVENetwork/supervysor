d550c2fda42c7eab08566269bf8980612add4057
christopherbrumm <christopherbrumm2000@gmail.com>
2023-05-26 11:44:48 +0200

    update: 1

--- cmd/supervysor/commands/start.go
+++ cmd/supervysor/commands/start.go
@@ -36,17 +36,19 @@ var startCmd = &cobra.Command{
 
 		fmt.Println("STARTED INITIALLY.")
 
-		// Setup a channel to receive a signal
-		done := make(chan os.Signal, 1)
-
-		// Notify this channel when a SIGINT is received
-		signal.Notify(done, os.Interrupt)
-
 		// Fire off a goroutine to loop until that channel receives a signal.
 		// When a signal is received simply exit the program
 		go func() {
+			// Setup a channel to receive a signal
+			c := make(chan os.Signal, 1)
+
+			// Notify this channel when a SIGINT is received
+			signal.Notify(c, os.Interrupt)
+
+			<-c
+
 			fmt.Println("CTRL + C")
-			for _ = range done {
+			for _ = range c {
 				os.Exit(0)
 			}
 
