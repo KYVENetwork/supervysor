a26dd85842f45f80627cc63c94f8e11f39d3cf70
christopherbrumm <christopherbrumm2000@gmail.com>
2023-06-14 10:33:35 +0200

    chore: remove deprecated logger

--- cmd/supervysor/start.go
+++ cmd/supervysor/start.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
 	"time"
-
-	"github.com/KYVENetwork/supervysor/cmd/supervysor/helpers"
 
 	"github.com/KYVENetwork/supervysor/pool"
 
@@ -26,24 +21,8 @@ var startCmd = &cobra.Command{
 			return err
 		}
 
-		supervysorDir, err := helpers.GetSupervysorDir()
-		if err != nil {
-			logger.Error("could not load supervysor directory")
-			return err
-		}
-
-		logFile, err := os.OpenFile(supervysorDir+"/logs/"+time.Now().Format("20060102_150405")+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0o777)
-		if err != nil {
-			logger.Error(fmt.Sprintf("could not open log file: %v", err))
-			return err
-		}
-		defer logFile.Close()
-
-		fileLogger := log.New(logFile, "", log.LstdFlags)
-
 		// Start data source node initially.
 		if err := node.InitialStart(config.BinaryPath, config.AddrBookPath, config.Seeds); err != nil {
-			fileLogger.Printf("initial start failed: %s", err)
 			logger.Error("initial start failed", "err", err)
 			return err
 		}
@@ -68,7 +47,6 @@ var startCmd = &cobra.Command{
 				return err
 			}
 
-			fileLogger.Printf(fmt.Sprintf("fetched heights successfully; node=%d pool=%d current-target-height=%d", nodeHeight, poolHeight, *poolHeight+config.HeightDifferenceMax))
 			logger.Info("fetched heights successfully", "node", nodeHeight, "pool", poolHeight, "current-target-height", *poolHeight+config.HeightDifferenceMax)
 
 			// Calculate height difference to enable the correct mode.
@@ -77,7 +55,6 @@ var startCmd = &cobra.Command{
 			if heightDiff >= config.HeightDifferenceMax {
 				// Data source node has synced far enough, enable or keep Ghost Mode
 				if err = node.EnableGhostMode(config.BinaryPath, config.AddrBookPath); err != nil {
-					fileLogger.Printf(fmt.Sprintf("could not enable Ghost Mode err=%s", err))
 					logger.Error("could not enable Ghost Mode", "err", err)
 
 					if shutdownErr := node.ShutdownNode(); shutdownErr != nil {
@@ -87,7 +64,6 @@ var startCmd = &cobra.Command{
 				}
 			} else if heightDiff < config.HeightDifferenceMax && heightDiff > config.HeightDifferenceMin {
 				// No threshold reached, keep current mode
-				fileLogger.Printf(fmt.Sprintf("keeping current Mode: height-difference=%d", heightDiff))
 				logger.Info("keeping current Mode", "height-difference", heightDiff)
 			} else {
 				// Difference is < HeightDifferenceMin, Data source needs to catch up, enable or keep Normal Mode
@@ -101,7 +77,6 @@ var startCmd = &cobra.Command{
 				}
 				// Diff < 0, can't use node as data source
 				if heightDiff <= 0 {
-					fileLogger.Printf("node has not reached pool height yet, can not use it as data source")
 					logger.Info("node has not reached pool height yet, can not use it as data source")
 				}
 			}
