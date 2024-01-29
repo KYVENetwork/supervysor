13e0a0d30c4df1033ad36b606e6be92dc01b09b9
christopherbrumm <christopherbrumm2000@gmail.com>
2024-01-29 14:00:42 +0100

    update: support any db.

--- store/prune.go
+++ store/prune.go
@@ -2,73 +2,24 @@ package store
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
+	"strings"
 
-	"github.com/syndtr/goleveldb/leveldb/opt"
-	"github.com/tendermint/tendermint/state"
+	"github.com/KYVENetwork/supervysor/utils"
 
 	"cosmossdk.io/log"
-
-	tmStore "github.com/tendermint/tendermint/store"
-	db "github.com/tendermint/tm-db"
 )
 
 func Prune(home string, untilHeight int64, statePruning bool, logger log.Logger) error {
-	o := opt.Options{
-		DisableSeeksCompaction: true,
-	}
-
-	// Get BlockStore
-	blockStoreDB, err := db.NewGoLevelDBWithOpts("blockstore", filepath.Join(home, "data"), &o)
+	config, err := utils.LoadConfig(home)
 	if err != nil {
-		return fmt.Errorf("failed to create blockStoreDB: %w", err)
-	}
-	blockStore := tmStore.NewBlockStore(blockStoreDB)
-
-	// Get StateStore
-	stateDB, err := db.NewGoLevelDBWithOpts("state", filepath.Join(home, "data"), &o)
-	if err != nil {
-		return fmt.Errorf("failed to create stateStoreDB: %w", err)
-	}
-	stateStore := state.NewStore(stateDB)
-
-	base := blockStore.Base()
-
-	logger.Info("blockstore base", "base", base)
-
-	if untilHeight < base {
-		logger.Error("Error: base height is higher than prune height", "base height", base, "until height", untilHeight)
-		os.Exit(0)
+		return fmt.Errorf("failed to load config: %w", err)
 	}
 
-	blocks, err := blockStore.PruneBlocks(untilHeight)
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(0)
-	}
-
-	if err = blockStoreDB.ForceCompact(nil, nil); err != nil {
-		logger.Error(err.Error())
-		os.Exit(0)
-	}
-
-	if statePruning {
-		err = stateStore.PruneStates(base, untilHeight)
-		if err != nil {
-			logger.Error(err.Error())
-			os.Exit(0)
-		}
-
-		if err = stateDB.ForceCompact(nil, nil); err != nil {
-			logger.Error(err.Error())
-			os.Exit(0)
-		}
-
-		logger.Info(fmt.Sprintf("Pruned %d blocks and the state until %d, new base height is %d", blocks, untilHeight, blockStore.Base()))
+	if strings.ToLower(config.DBBackend) == "goleveldb" {
+		logger.Info("GoLevelDB detected, using ForceCompact")
+		return PruneGoLevelDB(home, untilHeight, statePruning, logger)
 	} else {
-		logger.Info(fmt.Sprintf("Pruned %d blocks until %d, new base height is %d", blocks, untilHeight, blockStore.Base()))
+		logger.Info("another DB backend than GoLevelDB detected, ForceCompact not supported")
+		return PruneAnyDB(home, untilHeight, statePruning, logger)
 	}
-
-	return nil
 }
